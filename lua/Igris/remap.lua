5555a0fd1ef0871f8c860185d12224b9d2f93194
IgrisBRC <igris4884@gmail.com>
2024-09-28 10:58:52 +0530

    tab binds for the tmux haters

--- lua/Igris/remap.lua
+++ lua/Igris/remap.lua
@@ -1,11 +1,11 @@
 vim.g.mapleader = " "
 vim.keymap.set("n","<leader>t",vim.cmd.Ex)
 
-vim.keymap.set("n","<C-b>n",":tabnew<CR>")
+vim.keymap.set("n","<C-t>n",":tabnew<CR>")
 
-vim.keymap.set("n","<C-b>1",":tabn 1<CR>")
-vim.keymap.set("n","<C-b>2",":tabn 2<CR>")
-vim.keymap.set("n","<C-b>3",":tabn 3<CR>")
-vim.keymap.set("n","<C-b>4",":tabn 4<CR>")
-vim.keymap.set("n","<C-b>5",":tabn 5<CR>")
-vim.keymap.set("n","<C-b>6",":tabn 6<CR>")
+vim.keymap.set("n","<C-t>1",":tabn 1<CR>")
+vim.keymap.set("n","<C-t>2",":tabn 2<CR>")
+vim.keymap.set("n","<C-t>3",":tabn 3<CR>")
+vim.keymap.set("n","<C-t>4",":tabn 4<CR>")
+vim.keymap.set("n","<C-t>5",":tabn 5<CR>")
+vim.keymap.set("n","<C-t>6",":tabn 6<CR>")
